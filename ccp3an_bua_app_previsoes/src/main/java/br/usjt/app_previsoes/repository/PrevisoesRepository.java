a22ed34f7878e70241c61d7e420cb70abdbcbab8
Wellington Viana <w.vianasouzaa@gmail.com>
2020-04-13 21:28:13 -0300

    removendo projeto errado

--- ccp3an_bua_app_previsoes/src/main/java/br/usjt/app_previsoes/repository/PrevisoesRepository.java
+++
@@ -1,8 +0,0 @@
-package br.usjt.app_previsoes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import br.usjt.app_previsoes.model.Previsao;
-
-public interface PrevisoesRepository extends JpaRepository<Previsao, Long> {
-
-}
